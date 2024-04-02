60ba4b46e20a29609788256fbbc5cf7f88840585
prachihirekha <prachihirekhan32@gmail.com>
2024-04-02 17:15:48 +0530

    Type casting

---
+++ src/main/java/practice/basic/Implicit.java
@@ -0,0 +1,9 @@
+package practice.basic;
+// implicit type casting
+public class Implicit {
+    public static void main(String[] args) {
+        int a =10;//4byte
+        double b =a;//8byte
+        System.out.println(b);
+    }
+}
