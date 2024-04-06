d81ec7ecd6673c106b48560371d7222517bc5623
prachihirekha <prachihirekhan32@gmail.com>
2024-04-06 16:21:05 +0530

    Assignment operator

---
+++ src/main/java/practice/operators/Assignment.java
@@ -0,0 +1,16 @@
+package practice.operators;
+
+public class Assignment {
+    public static void main(String[] args) {
+        int a;
+        // for assign a value;
+        a=10;
+        System.out.println(a);
+        //compound assignment operator
+        a+=10;// (a=a+10)
+        System.out.println(a);
+
+        a-=10;//(a=a-10)
+        System.out.println(a);
+    }
+}
